656b34aaead804d06cf537d081d52b2fa6826be6
zhangqt <>
2022-08-19 16:04:40 +0800

    UI

--- app/src/main/java/com/android/test/MainActivity.kt
+++ app/src/main/java/com/android/test/MainActivity.kt
@@ -69,6 +69,16 @@ class MainActivity : AppCompatActivity() {
                 appSelectDialogFragment.show(supportFragmentManager, "AppSelect4AndroidObb")
             }
         }
+
+        findViewById<View>(R.id.myApp).setOnClickListener {
+            val pkg = "com.folderv.file"
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$pkg")))
+            } catch (e: Exception) {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$pkg")))
+            }
+        }
+
     }
 
     @Synchronized
